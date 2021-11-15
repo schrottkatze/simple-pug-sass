414370218c8c89ef6daf25f19551d4b55cfb8a8c
Gabriel <68819302+obsidianical@users.noreply.github.com>
2021-11-15 09:19:17 +0100

    added some demo ts code

--- src/scripts/app.ts
+++ src/scripts/app.ts
@@ -1,1 +1,14 @@
 import "../pages/index.pug";
+
+let el = document.querySelector("#click-me");
+let flag = false;
+
+if (el) {
+  el.addEventListener("click", toggle);
+}
+
+function toggle() {
+  if (flag) document.body.style.backgroundColor = "#101010";
+  else document.body.style.backgroundColor = "#ff0000";
+  flag = !flag;
+}
